f59f6c05a1434afd2e9eb4ecc94fece983efc6aa
Neo MTIMKULU <nmtimkul@e5r9p10.wethinkcode.co.za>
2017-10-01 11:17:56 +0200

    -a

--- get_next_line.c
+++ get_next_line.c
@@ -5,106 +5,59 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmtimkul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/02 09:44:00 by nmtimkul          #+#    #+#             */
-/*   Updated: 2017/08/10 12:32:44 by nmtimkul         ###   ########.fr       */
+/*   Created: 2017/09/21 11:58:48 by nmtimkul          #+#    #+#             */
+/*   Updated: 2017/10/01 11:16:43 by nmtimkul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "get_next_line.h"
+#include "libft/libft.h"
 
-static t_buff	read_buffer(const int fd, t_buff b)
+static int		get_content(const int fd, char **line)
 {
-	long int			k;
+	char	*buff;
+	char	*temp;
+	int		data;
 
-	b.buf_pos = 0;
-	k = -1;
-	b.buf_fd = fd;
-	b.eof = read(fd, b.buff, BUFF_SIZE);
-	if (b.eof < BUFF_SIZE && b.eof > 0)
+	if (!(buff = (char *)malloc(sizeof(*buff) * (BUFF_SIZE + 1))))
+		return (-1);
+	data = read(fd, buff, BUFF_SIZE);
+	if (data > 0)
 	{
-		k = b.eof - 1;
-		while (k++ <= BUFF_SIZE)
-			b.buff[k] = '\0';
+		buff[data] = '\0';
+		temp = ft_strjoin(*line, buff);
+		free(*line);
+		*line = temp;
 	}
-	return (b);
-}
-
-static char		*re_malloc(char *line, size_t size)
-{
-	char			*tmp;
-
-	tmp = ft_strnew(size + L_LEN);
-	if (size > 0)
-	{
-		tmp = ft_strcpy(tmp, line);
-		free(line);
-	}
-	return (tmp);
-}
-
-static t_buff	get_line(const int fd, char **line, t_buff b)
-{
-	unsigned int	line_pos;
-	char			*l;
-
-	line_pos = 0;
-	if (fd != b.buf_fd || b.buf_pos >= BUFF_SIZE || b.eof < 0)
-		b = read_buffer(fd, b);
-	while (b.buff[b.buf_pos] != '\n' && b.buf_pos < b.eof && b.eof > -1)
-	{
-		if (((line_pos + 1) % L_LEN == 0) || line_pos == 0)
-			l = re_malloc(l, line_pos);
-		l[line_pos] = b.buff[b.buf_pos];
-		line_pos++;
-		b.buf_pos++;
-		if (b.buf_pos >= BUFF_SIZE)
-			b = read_buffer(fd, b);
-	}
-	if ((b.eof < BUFF_SIZE && b.eof != -1) && (b.eof <= b.buf_pos))
-		b.eof = -2;
-	b.buf_pos++;
-	if (line_pos == 0)
-		*line = 0;
-	else
-		*line = l;
-	return (b);
-}
-
-static int		get_buff(const int fd, char **line)
-{
-	static t_buff	arr[NUM_FILES];
-	long int		k;
-
-	k = 0;
-  	while (k < NUM_FILES && fd != arr[k].buf_fd)
-		k++;
-	if (k == NUM_FILES)
-		k--;
-	if (arr[k].buf_fd == fd)
-		arr[k] = get_line(fd, line, arr[k]);
-	else
-	{
-		while (k >= 0 && arr[k].buf_fd != 0 && arr[k].eof > -1)
-			k--;
-		if (k >= 0)
-			arr[k] = get_line(fd, line, arr[k]);
-		else
-			arr[k] = get_line(fd, line, arr[NUM_FILES - 1]);
-	}
-	return (arr[k].eof);
+	free(buff);
+	return (data);
 }
 
 int				get_next_line(const int fd, char **line)
 {
-	long int	eof;
+	static	char	*stack = NULL;
+	char			*buff;
+	int				ret;
 
-	if (BUFF_SIZE > MAX_BUFF || BUFF_SIZE < 1)
+	if ((!stack && (stack = (char *)malloc(sizeof(*stack))) == NULL) ||
+			!line || fd < 0 || BUFF_SIZE < 0)
 		return (-1);
-	eof = get_buff(fd, line);
-	if (eof == -1)
-		return (-1);
-	if (eof == -2)
-		return (0);
+	buff = ft_strchr(stack, '\n');
+	while (buff == NULL)
+	{
+		ret = get_content(fd, &stack);
+		if (ret == 0)
+		{
+			if (ft_strlen(stack) == 0)
+				return (0);
+			stack = ft_strjoin(stack, "\n");
+		}
+		if (ret < 0)
+			return (-1);
+		else
+			buff = ft_strchr(stack, '\n');
+	}
+	*line = ft_strsub(stack, 0, ft_strlen(stack) - ft_strlen(buff));
+	stack = ft_strdup(buff + 1);
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,32 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmtimkul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/02 09:44:14 by nmtimkul          #+#    #+#             */
-/*   Updated: 2017/08/10 12:33:02 by nmtimkul         ###   ########.fr       */
+/*   Created: 2017/09/21 12:16:23 by nmtimkul          #+#    #+#             */
+/*   Updated: 2017/10/01 11:16:57 by nmtimkul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
 
-# define BUFF_SIZE 64
-# define L_LEN 50
-# define NUM_FILES 10
-# define MAX_BUFF 800000
-
-# include <sys/types.h>
-# include <sys/uio.h>
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
 
-int					get_next_line(const int fd, char **line);
-
-typedef struct		s_buff
-{
-	char			buff[BUFF_SIZE];
-	int				buf_pos;
-	int				buf_fd;
-	int				eof;
-}					t_buff;
+int			get_next_line(const int fd, char **line);
 
 #endif
